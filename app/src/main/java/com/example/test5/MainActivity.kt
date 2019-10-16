f7bd1967680485c3908c36cf014db1f1bea3a081
skadmsfl <noeytt.pk@gmail.com>
2019-10-16 19:09:02 +0700

    operand1

--- app/src/main/java/com/example/test5/MainActivity.kt
+++ app/src/main/java/com/example/test5/MainActivity.kt
@@ -80,7 +80,28 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun performOperation(value: String, operation: String) {
-        displayOperation.text = operation
+        if (operand1 == null) {
+            operand1 = value.toDouble()
+        } else {
+            operand2 = value.toDouble()
+
+            if (pendingOperation == "=") {
+                displayOperation.text = operation
+            }
+            when (pendingOperation) {
+                "=" -> operand1 = operand2
+                "/" -> if (operand2 == 0.0) {
+                    operand1 = Double.NaN   // handle attempt to divide by zero
+                } else {
+                    operand1 = operand1!! / operand2
+                }
+                "*" -> operand1 = operand1!! * operand2
+                "-" -> operand1 = operand1!! - operand2
+                "+" -> operand1 = operand1!! + operand2
+            }
+        }
+        result.setText(operand1.toString())
+        newNumber.setText("")
 
     }
 }
